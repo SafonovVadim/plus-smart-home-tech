bb0ab9b48a9c0001fda4ea5c8117166fde48cb28
Сафонов Вадим Андреевич (21648638) <safonov.v.a@sbertech.ru>
2026-07-06 15:12:24 +0300

    add config

--- telemetry/collector/src/main/java/ru/yandex/practicum/collector/config/KafkaProducerConfig.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/collector/config/KafkaProducerConfig.java
@@ -5,12 +5,14 @@ import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.ByteArraySerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.util.Properties;
 
 @Configuration
+@ConfigurationProperties("collector")
 public class KafkaProducerConfig {
 
     @Value("${kafka.bootstrap-servers}")
