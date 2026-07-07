278d8c1f252d04f1a3a942d1191ba700527de572
Сафонов Вадим Андреевич (21648638) <safonov.v.a@sbertech.ru>
2026-07-07 17:59:06 +0300

    add config

--- infra/discovery-server/src/main/java/ru/yandex/practicum/DiscoveryServer.java
+++ infra/discovery-server/src/main/java/ru/yandex/practicum/DiscoveryServer.java
@@ -2,13 +2,12 @@ package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableConfigServer
 @SpringBootApplication
-public class ConfigServer {
-
+@EnableEurekaServer
+public class DiscoveryServer {
     public static void main(String[] args) {
-        SpringApplication.run(ConfigServer.class, args);
+        SpringApplication.run(DiscoveryServer.class, args);
     }
 }
