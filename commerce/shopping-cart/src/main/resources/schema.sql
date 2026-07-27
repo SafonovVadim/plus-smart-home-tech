48d3fc88134d7ab785d399d071dec4ab20ba4baf
Сафонов Вадим Андреевич (21648638) <safonov.v.a@sbertech.ru>
2026-07-27 15:54:20 +0300

    add microservices

--- commerce/shopping-cart/src/main/resources/schema.sql
+++ commerce/shopping-cart/src/main/resources/schema.sql
@@ -1,14 +1,17 @@
+-- schema.sql
 CREATE SCHEMA IF NOT EXISTS shopping_cart;
 
-CREATE TABLE IF NOT EXISTS shopping_cart.carts (
-    cart_id UUID PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS shopping_cart.carts
+(
+    cart_id  UUID PRIMARY KEY,
     username VARCHAR NOT NULL UNIQUE,
-    state VARCHAR NOT NULL
+    state    VARCHAR NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS shopping_cart.cart_products (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    cart_id UUID REFERENCES carts(cart_id) ON DELETE CASCADE,
-    product_id UUID NOT NULL,
-    quantity INTEGER NOT NULL
+CREATE TABLE IF NOT EXISTS shopping_cart.cart_products
+(
+    id         BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    cart_id    UUID    NOT NULL REFERENCES shopping_cart.carts (cart_id) ON DELETE CASCADE,
+    product_id UUID    NOT NULL,
+    quantity   INTEGER NOT NULL
 );
