423e43557d726720dcf4cf7a90c8960a9e951cdd
Сафонов Вадим Андреевич (21648638) <safonov.v.a@sbertech.ru>
2026-07-28 17:36:16 +0300

    add Circuit Breaker

--- commerce/shopping-cart/src/main/java/ru/yandex/practicum/ShoppingCartApplication.java
+++ commerce/shopping-cart/src/main/java/ru/yandex/practicum/ShoppingCartApplication.java
@@ -3,13 +3,11 @@ package ru.yandex.practicum;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableCircuitBreaker
 @ConfigurationPropertiesScan
 @EnableDiscoveryClient
 public class ShoppingCartApplication {
